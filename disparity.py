842eb7b2239e9bbd9ce3d5bc66c16599b48a422d
Joe Minichino <joe.minichino@gmail.com>
2015-04-22 22:36:55 +0100

    edited grabcut and watershed

--- disparity.py
+++ disparity.py
@@ -1,88 +1,49 @@
-#!/usr/bin/env python
-
-'''
-Simple example of stereo image matching and point cloud generation.
-
-Resulting .ply file cam be easily viewed using MeshLab ( http://meshlab.sourceforge.net/ )
-'''
-
 import numpy as np
 import cv2
 
-ply_header = '''ply
-format ascii 1.0
-element vertex %(vert_num)d
-property float x
-property float y
-property float z
-property uchar red
-property uchar green
-property uchar blue
-end_header
-'''
-
-def write_ply(fn, verts, colors):
-    verts = verts.reshape(-1, 3)
-    colors = colors.reshape(-1, 3)
-    verts = np.hstack([verts, colors])
-    with open(fn, 'w') as f:
-        f.write(ply_header % dict(vert_num=len(verts)))
-        np.savetxt(f, verts, '%f %f %f %d %d %d')
-
 def update(val = 0):
-    # disparity range is tuned for 'aloe' image pair
-    stereo = cv2.StereoSGBM_create(
-        minDisparity = min_disp,
-        numDisparities = num_disp,
-        blockSize = cv2.getTrackbarPos('window_size', 'disparity'),
-        uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio', 'disparity'),
-        speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize', 'disparity'),
-        speckleRange = cv2.getTrackbarPos('speckleRange', 'disparity'),
-        disp12MaxDiff = 10,
-        P1 = 600,
-        P2 = 2400
-    )
+    stereo.setBlockSize(cv2.getTrackbarPos('window_size', 'disparity'))
+    stereo.setUniquenessRatio(cv2.getTrackbarPos('uniquenessRatio', 'disparity'))
+    stereo.setSpeckleWindowSize(cv2.getTrackbarPos('speckleWindowSize', 'disparity'))
+    stereo.setSpeckleRange(cv2.getTrackbarPos('speckleRange', 'disparity'))
+    stereo.setDisp12MaxDiff(cv2.getTrackbarPos('disp12MaxDiff', 'disparity'))
 
     print 'computing disparity...'
     disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
-
-    print 'generating 3d point cloud...',
-    h, w = imgL.shape[:2]
-    f = 0.8*w                          # guess for focal length
-    Q = np.float32([[1, 0, 0, -0.5*w],
-                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
-                    [0, 0, 0,     -f], # so that y-axis looks up
-                    [0, 0, 1,      0]])
-    points = cv2.reprojectImageTo3D(disp, Q)
-    colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
-    mask = disp > disp.min()
-    out_points = points[mask]
-    out_colors = colors[mask]
-    out_fn = 'out.ply'
-    write_ply('out.ply', out_points, out_colors)
-    print '%s saved' % 'out.ply'
 
     cv2.imshow('left', imgL)
     cv2.imshow('disparity', (disp-min_disp)/num_disp)
     
 
 if __name__ == "__main__":
-    window_size = 9
-    min_disp = -64
+    window_size = 5
+    min_disp = 16
     num_disp = 192-min_disp
     blockSize = window_size
-    uniquenessRatio = 7
-    speckleRange = 8
-    speckleWindowSize = 10
-    disp12MaxDiff = 10
+    uniquenessRatio = 1
+    speckleRange = 3
+    speckleWindowSize = 3
+    disp12MaxDiff = 200
     P1 = 600
     P2 = 2400
-    imgL = cv2.imread('images/stacked1.png')
-    imgR = cv2.imread('images/stacked2.png')    
+    imgL = cv2.imread('images/dis1.jpg')
+    imgR = cv2.imread('images/dis2.jpg')
     cv2.namedWindow('disparity')
     cv2.createTrackbar('speckleRange', 'disparity', speckleRange, 50, update)    
-    cv2.createTrackbar('window_size', 'disparity', window_size, 51, update)
+    cv2.createTrackbar('window_size', 'disparity', window_size, 21, update)
     cv2.createTrackbar('speckleWindowSize', 'disparity', speckleWindowSize, 200, update)
     cv2.createTrackbar('uniquenessRatio', 'disparity', uniquenessRatio, 50, update)
+    cv2.createTrackbar('disp12MaxDiff', 'disparity', disp12MaxDiff, 250, update)
+    stereo = cv2.StereoSGBM_create(
+        minDisparity = min_disp,
+        numDisparities = num_disp,
+        blockSize = window_size,
+        uniquenessRatio = uniquenessRatio,
+        speckleRange = speckleRange,
+        speckleWindowSize = speckleWindowSize,
+        disp12MaxDiff = disp12MaxDiff,
+        P1 = P1,
+        P2 = P2
+    )
     update()
     cv2.waitKey()
