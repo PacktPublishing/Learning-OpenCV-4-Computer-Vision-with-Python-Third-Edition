1f235303a7b5bc6359c71702cbadc833dc365142
Joe Minichino <joe.minichino@gmail.com>
2015-04-28 21:48:59 +0100

    chessboard corners

--- corner.py
+++ corner.py
@@ -1,18 +1,13 @@
 import cv2
 import numpy as np
 
-camera = cv2.VideoCapture(0)
-cv2.namedWindow('dst')
+img = cv2.imread('images/chess_board.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray, 9, 23, 0.04)
+img[dst>0.01 * dst.max()] = [0, 0, 255] 
 while (True):
-  
-  ret, frame = camera.read()
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  gray = np.float32(gray)
-  dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-  frame[dst>0.01 * dst.max()] = [0, 0, 255]
-  cv2.imshow('dst', frame)
+  cv2.imshow('corners', img)
   if cv2.waitKey(1000 / 12) & 0xff == ord("q"):
     break
-
-camera.release()
 cv2.destroyAllWindows()
