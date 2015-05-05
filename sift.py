ca05d9d4caafeff07ab208d5f2d33a84f43d35ff
Joe Minichino <joe.minichino@gmail.com>
2015-05-05 20:28:12 +0100

    sift working

--- sift.py
+++ sift.py
@@ -8,12 +8,11 @@ img = cv2.imread(imgpath)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 sift = cv2.xfeatures2d.SIFT_create()
-kp = sift.detectAndCompute(gray,None)
+keypoints, descriptor = sift.detectAndCompute(gray,None)
 
-img = cv2.drawKeypoints(image=gray, outImage=img, keypoints = kp, flags = 4, color = (51, 103, 236))
-cv2.imwrite('sift_keypoints.jpg', img)
+img = cv2.drawKeypoints(image=gray, outImage=img, keypoints = keypoints, flags = 4, color = (51, 163, 236))
 
-cv2.imshow('sift_keypoints.jpg', cv2.imread('sift_keypoints.jpg'))
+cv2.imshow('sift_keypoints', img)
 while (True):
   if cv2.waitKey(1000 / 12) & 0xff == ord("q"):
     break
