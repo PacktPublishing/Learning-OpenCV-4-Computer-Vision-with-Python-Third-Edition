f59e442afbca6dd36d54502a14153afb76cd7903
Joe Minichino <joe.minichino@gmail.com>
2015-05-05 22:01:55 +0100

    surf working

--- sift.py
+++ sift.py
@@ -10,7 +10,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 sift = cv2.xfeatures2d.SIFT_create()
 keypoints, descriptor = sift.detectAndCompute(gray,None)
 
-img = cv2.drawKeypoints(image=gray, outImage=img, keypoints = keypoints, flags = 4, color = (51, 163, 236))
+img = cv2.drawKeypoints(image=img, outImage=img, keypoints = keypoints, flags = 4, color = (51, 163, 236))
 
 cv2.imshow('sift_keypoints', img)
 while (True):
