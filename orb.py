b73196e79d5fb33928f95053f8d22f9c4d783e5e
Joe Minichino <joe.minichino@gmail.com>
2015-05-11 18:06:13 +0100

    added images and orb

--- orb.py
+++ orb.py
@@ -5,23 +5,15 @@ from matplotlib import pyplot as plt
 img1 = cv2.imread('images/manowar_logo.png',0) # queryImage
 img2 = cv2.imread('images/manowar_single.jpg',0) # trainImage
 
-# Initiate SIFT detector
 orb = cv2.ORB_create()
-
-# find the keypoints and descriptors with SIFT
 kp1, des1 = orb.detectAndCompute(img1,None)
 kp2, des2 = orb.detectAndCompute(img2,None)
 
-# create BFMatcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-# Match descriptors.
 matches = bf.match(des1,des2)
 
 # Sort them in the order of their distance.
 matches = sorted(matches, key = lambda x:x.distance)
-
-# Draw first 10 matches.
-img3 = cv2.drawMatches(img1,kp1,img2,kp2, matches[:40], img2,flags=2)
+img3 = cv2.drawMatches(img1,kp1,img2,kp2, matches[:25], img2,flags=2)
 
 plt.imshow(img3),plt.show()
