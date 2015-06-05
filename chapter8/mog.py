c8a2c57302c7e17033ff7dd7534ccb517435177e
Joe Minichino <joe.minichino@gmail.com>
2015-06-05 21:25:24 +0100

    meanshift

--- chapter8/mog.py
+++ chapter8/mog.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 
 bs = cv2.createBackgroundSubtractorKNN(detectShadows = True)
-
-camera = cv2.VideoCapture("/home/d3athmast3r/Downloads/traffic.flv")
+camera = cv2.VideoCapture("movie.mpg")
 
 while True:
   ret, frame = camera.read()
   fgmask = bs.apply(frame)
   th = cv2.threshold(fgmask.copy(), 244, 255, cv2.THRESH_BINARY)[1]
-  dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3)), iterations = 2)
+  th = cv2.erode(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3)), iterations = 2)
+  dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8,3)), iterations = 2)
   image, contours, hier = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   for c in contours:
-    if cv2.contourArea(c) > 1600:
+    if cv2.contourArea(c) > 1000:
       (x,y,w,h) = cv2.boundingRect(c)
       cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 255, 0), 2)
 
