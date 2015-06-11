6402659957adfc0e62c9ae338b3e8c43ece3987a
Joe Minichino <joe.minichino@gmail.com>
2015-06-11 20:28:12 +0100

    kalman example streamlined

--- chapter8/kalman.py
+++ chapter8/kalman.py
@@ -1,25 +1,28 @@
 import cv2, numpy as np
 
-measurements=[]
-predictions=[]
-frame = np.zeros((400,400,3), np.uint8) # the empty frame working as a canvas
-current_measurement = np.array((2,1), np.float32) # measurement array 
-current_prediction = np.zeros((2,1), np.float32) # tracked / prediction
+measurements = []
+predictions = []
+frame = np.zeros((800, 800, 3), np.uint8)
+last_measurement = current_measurement = np.array((2,1), np.float32) 
+last_prediction = current_prediction = np.zeros((2,1), np.float32)
 
-def onmouse(event, x, y, s, p):
-    global current_measurement, measurements
+def mousemove(event, x, y, s, p):
+    global frame, current_measurement, measurements, last_measurement, current_prediction, last_prediction
+    last_prediction = current_prediction
+    last_measurement = current_measurement
     current_measurement = np.array([[np.float32(x)],[np.float32(y)]])
-    measurements.append((x,y))
+    kalman.correct(current_measurement)
+    current_prediction = kalman.predict()
+    lmx, lmy = last_measurement[0], last_measurement[1]
+    cmx, cmy = current_measurement[0], current_measurement[1]
+    lpx, lpy = last_prediction[0], last_prediction[1]
+    cpx, cpy = current_prediction[0], current_prediction[1]
+    cv2.line(frame, (lmx, lmy), (cmx, cmy), (0,100,0))
+    cv2.line(frame, (lpx, lpy), (cpx, cpy), (0,0,200))
 
-def draw(frame, measurements, predictions):
-    for i in range(len(measurements)-1):
-        cv2.line(frame, measurements[i], measurements[i+1], (0,100,0))
 
-    for i in range(len(predictions)-1):
-        cv2.line(frame, predictions[i], predictions[i+1], (0,0,200))
-
-cv2.namedWindow("kalman")
-cv2.setMouseCallback("kalman",onmouse);
+cv2.namedWindow("kalman_tracker")
+cv2.setMouseCallback("kalman_tracker", mousemove);
 
 kalman = cv2.KalmanFilter(4,2)
 kalman.measurementMatrix = np.array([[1,0,0,0],[0,1,0,0]],np.float32)
@@ -27,12 +30,8 @@ kalman.transitionMatrix = np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],np.
 kalman.processNoiseCov = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32) * 0.03
 
 while True:
-    kalman.correct(current_measurement)
-    current_prediction = kalman.predict()
-    predictions.append((int(current_prediction[0]),int(current_prediction[1])))
-    draw(frame, measurements, predictions)
-    cv2.imshow("kalman",frame)
-    k = cv2.waitKey(30) &0xFF
-    if k == 27: break
+    cv2.imshow("kalman_tracker", frame)
+    if (cv2.waitKey(30) & 0xFF) == 27:
+        break
 
 cv2.destroyAllWindows()
