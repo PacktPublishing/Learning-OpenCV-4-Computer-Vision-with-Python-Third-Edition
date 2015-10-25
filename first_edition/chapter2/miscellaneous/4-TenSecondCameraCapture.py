552406d56ea35b8de229ed0db352cf72abfb266a
Joe Howse <josephhowse@nummist.com>
2015-10-25 17:46:31 -0300

    Using MJPG video format instead of I420

--- first_edition/chapter2/miscellaneous/4-TenSecondCameraCapture.py
+++ first_edition/chapter2/miscellaneous/4-TenSecondCameraCapture.py
@@ -5,7 +5,7 @@ fps = 30 # an assumption
 size = (int(cameraCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
         int(cameraCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 videoWriter = cv2.VideoWriter(
-    'MyOutputVid.avi', cv2.VideoWriter_fourcc('I','4','2','0'), fps, size)
+    'MyOutputVid.avi', cv2.VideoWriter_fourcc('M','J','P','G'), fps, size)
 
 success, frame = cameraCapture.read()
 numFramesRemaining = 10 * fps - 1
