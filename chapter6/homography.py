bd3fcd2206d94b9b82355d87a2a32a86be8be4be
Joe Howse <josephhowse@nummist.com>
2019-08-17 16:15:52 -0300

    Drawing homography result

--- chapter6/homography.py
+++ chapter6/homography.py
@@ -2,11 +2,11 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-MIN_MATCH_COUNT = 10
+MIN_NUM_GOOD_MATCHES = 10
 
 img0 = cv2.imread('tattoos/query.png',
                   cv2.IMREAD_GRAYSCALE)
-img1 = cv2.imread('tattoos/anchor_man.png',
+img1 = cv2.imread('tattoos/anchor-man.png',
                   cv2.IMREAD_GRAYSCALE)
 
 # Perform SIFT feature detection and description.
@@ -29,7 +29,7 @@ for m, n in matches:
     if m.distance < 0.7 * n.distance:
         good_matches.append(m)
 
-if len(good_matches) >= MIN_MATCH_COUNT:
+if len(good_matches) >= MIN_NUM_GOOD_MATCHES:
     src_pts = np.float32(
         [kp0[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
     dst_pts = np.float32(
@@ -38,21 +38,32 @@ if len(good_matches) >= MIN_MATCH_COUNT:
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
     mask_matches = mask.ravel().tolist()
 
-    h,w = img0.shape
-    pts = np.float32(
+    h, w = img0.shape
+    src_corners = np.float32(
         [[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
-    dst = cv2.perspectiveTransform(pts, M)
+    dst_corners = cv2.perspectiveTransform(src_corners, M)
+    dst_corners = dst_corners.astype(np.int32)
+
+    # Draw the bounds of the matched region based on the homography.
+    num_corners = len(dst_corners)
+    for i in range(num_corners):
+        x0, y0 = dst_corners[i][0]
+        if i == num_corners - 1:
+            next_i = 0
+        else:
+            next_i = i + 1
+        x1, y1 = dst_corners[next_i][0]
+        cv2.line(img1, (x0, y0), (x1, y1), 255, 3, cv2.LINE_AA)
+
+    # Draw the matches that passed the ratio test.
+    img_matches = cv2.drawMatches(
+        img0, kp0, img1, kp1, good_matches, None,
+        matchColor=(0, 255, 0), singlePointColor=None,
+        matchesMask=mask_matches, flags=2)
+
+    # Show the homography and good matches.
+    plt.imshow(img_matches)
+    plt.show()
 else:
-    print("Not enough matches are found - %d/%d" % \
+    print("Not enough matches good were found - %d/%d" % \
           (len(good_matches), MIN_MATCH_COUNT))
-    mask_matches = None
-
-# Draw the matches that passed the ratio test.
-img_matches = cv2.drawMatches(
-    img0, kp0, img1, kp1, good_matches, None,
-    matchColor=(0, 255, 0), singlePointColor=None,
-    matchesMask=mask_matches, flags=2)
-
-# Show the matches.
-plt.imshow(img_matches)
-plt.show()
