e9dd79123c3a5b29f52f0296b2877b38a51b1a8b
Joe Howse <josephhowse@nummist.com>
2019-09-25 16:13:28 -0300

    Updating chapter 7 samples

--- chapter7/detect_car_bow_svm_sliding_window.py
+++ chapter7/detect_car_bow_svm_sliding_window.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import os
 
+from car_detector.non_maximum import non_max_suppression_fast as nms
+
 if not os.path.isdir('CarData'):
     print('CarData folder not found. Please download and unzip '
           'http://l2r.cs.uiuc.edu/~cogcomp/Data/Car/CarData.tar.gz '
@@ -10,6 +12,9 @@ if not os.path.isdir('CarData'):
 
 BOW_NUM_TRAINING_SAMPLES_PER_CLASS = 10
 SVM_NUM_TRAINING_SAMPLES_PER_CLASS = 100
+
+SVM_SCORE_THRESHOLD = 1.0
+NMS_OVERLAP_THRESHOLD = 0.25
 
 sift = cv2.xfeatures2d.SIFT_create()
 
@@ -60,6 +65,24 @@ svm = cv2.ml.SVM_create()
 svm.train(np.array(training_data), cv2.ml.ROW_SAMPLE,
           np.array(training_labels))
 
+# TODO
+
+def pyramid(img, scale_factor=1.25, min_size=(200, 80)):
+    h, w = image.shape
+    min_w, min_h = min_size
+    while w >= min_w and h >= min_h:
+        yield image
+        w //= scale_factor
+        h //= scale_factor
+        img = cv2.resize(img, (w, h), interpolation = cv2.INTER_AREA)
+
+def sliding_window(img, step=20, window_size=(100, 40)):
+    img_h, img_w = img.shape
+    window_w, window_h = window_size
+    for y in range(0, img_w - window_w - (img_w % window_w) + 1, step):
+        for x in range(0, img_h - window_h - (img_h % window_h) + 1, step):
+            yield (x, y, img[y:y+window_h, x:x+window_w])
+
 for test_img_path in ['CarData/TestImages/test-0.pgm',
                       'CarData/TestImages/test-1.pgm',
                       '../images/car.jpg',
@@ -68,15 +91,29 @@ for test_img_path in ['CarData/TestImages/test-0.pgm',
                       '../images/woodcutters.jpg']:
     img = cv2.imread(test_img_path)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    descriptors = extract_bow_descriptors(gray_img)
-    prediction = svm.predict(descriptors)
-    if prediction[1][0][0] == 1.0:
-        text = 'car'
-        color = (0, 255, 0)
-    else:
-        text = 'not car'
-        color = (0, 0, 255)
-    cv2.putText(img, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                color, 2, cv2.LINE_AA)
+    pos_rects = []
+    for resized in pyramid(gray_img):
+        for x, y, roi in sliding_window(resized):
+            descriptors = extract_bow_descriptors(roi)
+            prediction = svm.predict(descriptors)
+            if prediction[1][0][0] == 1.0:
+                raw_prediction = svm.predict(
+                    descriptors, flags=cv2.ml.STAT_MODEL_RAW_OUTPUT)
+                score = -raw_prediction[1][0][0]
+                if score > SVM_SCORE_THRESHOLD:
+                    h, w = roi.shape
+                    scale = gray_img.shape[0] / float(resized.shape[0])
+                    pos_rects.append([int(x * scale),
+                                      int(y * scale),
+                                      int((x+w) * scale),
+                                      int((y+h) * scale),
+                                      score])
+    nms(np.array(pos_rects), NMS_OVERLAP_THRESHOLD)
+    for x0, y0, x1, y1, score in pos_rects:
+        cv2.rectangle(img, (int(x0), int(y0)), (int(x1), int(y1)),
+                      (0, 255, 0), 1)
+        text = '%.2f' % score
+        cv2.putText(img, text, (x, y - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
     cv2.imshow(test_img_path, img)
 cv2.waitKey(0)
