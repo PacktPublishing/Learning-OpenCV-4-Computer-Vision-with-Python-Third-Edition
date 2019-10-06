85f357111cc38d39357a3e98d2a6c3280db7f126
Joe Howse <josephhowse@nummist.com>
2019-10-06 11:37:47 -0300

    Updating chapter 8 samples

--- chapter8/mog.py
+++ chapter8/mog.py
@@ -1,28 +1,32 @@
 import cv2
-import numpy as np
 
-bs = cv2.createBackgroundSubtractorKNN(detectShadows = True)
-camera = cv2.VideoCapture("movie.mpg")
+bg_subtractor = cv2.createBackgroundSubtractorKNN(detectShadows=True)
+cap = cv2.VideoCapture('movie.mpg')
 
-while True:
-  ret, frame = camera.read()
-  fgmask = bs.apply(frame)
-  th = cv2.threshold(fgmask.copy(), 244, 255, cv2.THRESH_BINARY)[1]
-  th = cv2.erode(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3)), iterations = 2)
-  dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8,3)), iterations = 2)
-  image, contours, hier = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-  for c in contours:
-    if cv2.contourArea(c) > 1000:
-      (x,y,w,h) = cv2.boundingRect(c)
-      cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 255, 0), 2)
+success, frame = cap.read()
+while success:
 
-  cv2.imshow("mog", fgmask)
-  cv2.imshow("thresh", th)
-  cv2.imshow("diff", frame & cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR))
-  cv2.imshow("detection", frame)
-  k = cv2.waitKey(30) & 0xff
-  if k == 27:
-      break
+    fg_mask = bg_subtractor.apply(frame)
 
-camera.release()
-cv2.destroyAllWindows()
+    _, thresh = cv2.threshold(fg_mask, 244, 255, cv2.THRESH_BINARY)
+    erode_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    cv2.erode(thresh, erode_kernel, thresh, iterations=2)
+    dilate_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    cv2.dilate(thresh, dilate_kernel, thresh, iterations=2)
+
+    contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
+                                      cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) > 1000:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+    cv2.imshow('mog', fg_mask)
+    cv2.imshow('thresh', thresh)
+    cv2.imshow('detection', frame)
+
+    k = cv2.waitKey(30)
+    if k == 27:
+        break
+
+    success, frame = cap.read()
--- chapter8/trutils/utils.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-
-
-
