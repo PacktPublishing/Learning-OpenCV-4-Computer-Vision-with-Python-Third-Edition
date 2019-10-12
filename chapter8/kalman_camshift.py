bccaedb063a30f823bd90b7168a05d14fc791f97
Joe Howse <josephhowse@nummist.com>
2019-10-12 15:38:05 -0300

    Updating chapter 8 samples

--- chapter8/kalman_camshift.py
+++ chapter8/kalman_camshift.py
@@ -1,59 +1,90 @@
-import numpy as np
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-# take first frame of the video
-ret,frame = cap.read()
+# Capture several frames to allow the camera's autoexposure to adjust.
+for i in range(10):
+    success, frame = cap.read()
+if not success:
+    exit(1)
 
-# setup initial location of window
-r,h,c,w = 300,200,400,300  # simply hardcoded the values
-track_window = (c,r,w,h)
+# Define an initial tracking window in the center of the frame.
+frame_h, frame_w = frame.shape[:2]
+w = frame_w//8
+h = frame_h//8
+x = frame_w//2 - w//2
+y = frame_h//2 - h//2
+track_window = (x, y, w, h)
+
+# Calculate the normalized HSV histogram of the initial window.
+roi = frame[y:y+h, x:x+w]
+hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+mask = None
+roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+
+# Define the termination criteria:
+# 10 iterations or convergence within 1-pixel radius.
+term_crit = (cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10, 1)
+
+# Initialize the Kalman filter.
+kalman = cv2.KalmanFilter(4, 2)
+kalman.measurementMatrix = np.array(
+    [[1, 0, 0, 0],
+     [0, 1, 0, 0]], np.float32)
+kalman.transitionMatrix = np.array(
+    [[1, 0, 1, 0],
+     [0, 1, 0, 1],
+     [0, 0, 1, 0],
+     [0, 0, 0, 1]], np.float32)
+kalman.processNoiseCov = np.array(
+    [[1, 0, 0, 0],
+     [0, 1, 0, 0],
+     [0, 0, 1, 0],
+     [0, 0, 0, 1]], np.float32) * 0.03
+cx = x+w/2
+cy = y+h/2
+kalman.statePre = np.array(
+    [[cx], [cy], [0], [0]], np.float32)
+kalman.statePost = np.array(
+    [[cx], [cy], [0], [0]], np.float32)
 
 
-roi = frame[r:r+h, c:c+w]
-hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv_roi, np.array((160., 30.,32.)), np.array((180.,120.,255.)))
-roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+success, frame = cap.read()
+while success:
 
-kalman = cv2.KalmanFilter(4,2)
-kalman.measurementMatrix = np.array([[1,0,0,0],[0,1,0,0]],np.float32)
-kalman.transitionMatrix = np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],np.float32)
-kalman.processNoiseCov = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32) * 0.03
+    # Perform back-projection of the HSV histogram onto the frame.
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    back_proj = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
 
-measurement = np.array((2,1), np.float32) 
-prediction = np.zeros((2,1), np.float32)
+    # Perform tracking with CamShift.
+    rotated_rect, track_window = cv2.CamShift(
+        back_proj, track_window, term_crit)
 
-def center(points):
-    x = (points[0][0] + points[1][0] + points[2][0] + points[3][0]) / 4.0
-    y = (points[0][1] + points[1][1] + points[2][1] + points[3][1]) / 4.0
-    return np.array([np.float32(x), np.float32(y)], np.float32)
+    kalman.predict()
+    box_points = cv2.boxPoints(rotated_rect)
+    box_points = np.int0(box_points)
+    (cx, cy), radius = cv2.minEnclosingCircle(box_points)
+    center = np.array([cx, cy], np.float32)
+    estimate = kalman.correct(center)
+    center_offset = estimate[:,0][:2] - center
 
-while(1):
-    ret ,frame = cap.read()
+    # Draw the tracking window.
+    track_window = (track_window[0] + int(center_offset[0]),
+                    track_window[1] + int(center_offset[1]),
+                    track_window[2],
+                    track_window[3])
+    cv2.polylines(frame, [box_points], True, (255, 0, 0), 2)
 
-    if ret == True:
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-        
-        ret, track_window = cv2.CamShift(dst, track_window, term_crit)
-        
-        pts = cv2.boxPoints(ret)
-        pts = np.int0(pts)
-        (cx, cy), radius = cv2.minEnclosingCircle(pts)
-        kalman.correct(center(pts))
-        img2 = cv2.polylines(frame,[pts],True, 255,2)
-        prediction = kalman.predict()
-        cv2.circle(frame, (prediction[0], prediction[1]), int(radius), (0, 255, 0))
-        cv2.imshow('img2',img2)
-        k = cv2.waitKey(60) & 0xff
-        if k == 27:
-            break
+    cv2.imshow('camshift', frame)
 
-    else:
+    k = cv2.waitKey(1)
+    if k == 27:
         break
+
+    success, frame = cap.read()
+
 
 cv2.destroyAllWindows()
 cap.release()
--- chapter8/lk.py
+++ chapter8/lk.py
@@ -1,57 +1,65 @@
-import numpy as np
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
+# Capture several frames to allow the camera's autoexposure to adjust.
+for i in range(10):
+    success, old_frame = cap.read()
+if not success:
+    exit(1)
 
-# Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 2,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+# Find the initial features using Shi-Tomasi corner detection.
+old_gray_frame = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+old_points = cv2.goodFeaturesToTrack(
+    old_gray_frame, maxCorners=100, qualityLevel=0.3, minDistance=7,
+    blockSize=7)
 
-# Create some random colors
-color = np.random.randint(0,255,(100,3))
+# Define the Lukas-Kanade optical flow's termination criteria:
+# 10 iterations or convergence within 0.03-pixel radius.
+term_crit = (cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS,
+             10, 0.03)
 
-# Take first frame and find corners in it
-ret, old_frame = cap.read()
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+# Create an overlay to use in drawing motion trails.
+overlay = np.zeros_like(old_frame)
 
-# Create a mask image for drawing purposes
-mask = np.zeros_like(old_frame)
+# Create random colors to use in drawing motion trails.
+colors = np.random.randint(0, 255, (100, 3))
 
-while(1):
-    ret,frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+success, frame = cap.read()
+while(success):
 
-    # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
+    # Calculate the Lucas-Kanade optical flow.
+    points, statuses, distances = cv2.calcOpticalFlowPyrLK(
+         old_gray_frame, gray_frame, old_points, None,
+         winSize=(15, 15), maxLevel=2, criteria=term_crit)
 
-    # draw the tracks
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    img = cv2.add(frame,mask)
+    # Select the points that were successfully tracked.
+    good_points = points[statuses==1]
+    good_old_points = old_points[statuses==1]
 
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
+    # Draw and show the motion trails.
+    for i, (point, old_point) in enumerate(
+            zip(good_points, good_old_points)):
+        a, b = point.ravel()
+        c, d = old_point.ravel()
+        color = colors[i].tolist()
+        cv2.line(overlay, (a, b), (c, d), color, 2)
+        cv2.circle(frame, (a, b), 5, color, cv2.FILLED)
+    cv2.add(frame, overlay, frame)
+    cv2.imshow('lk', frame)
+
+    k = cv2.waitKey(1)
     if k == 27:
         break
 
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
+    # Update the previous frame and points.
+    old_gray_frame = gray_frame
+    old_points = good_points.reshape(-1, 1, 2)
+
+    success, frame = cap.read()
 
 cv2.destroyAllWindows()
 cap.release()
